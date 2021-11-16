e19a835db98d789a52291bc99e57d1cf71a68a0a
Jennifer <jenniferh7637@gmail.com>
2021-11-16 19:23:50 -0300

    alterada rota com parametros

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get("/segunda", function(req, res) {
     res.send("Minha 2ª requisição")
 })
 
+app.get("/com-parametros", function(req, res) {
+    if (req.query.nome === "Jennifer") {
+        res.send("Jennifer chamou requisição")
+    }
+    res.send("Com parâmetros funciona! Sabadou, " + req.query.nome + req.query.sobrenome)
+})
+
 app.listen(port, function() {
     console.log("Ouvindo a porta: ", port);
 })
