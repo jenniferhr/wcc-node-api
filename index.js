30e51c49effa6dd6586c13d3acf4943ae43b3cf7
Jennifer <jenniferh7637@gmail.com>
2021-11-16 21:39:36 -0300

    minhas primeiras rotas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+//npm install --save-dev nodemon
+
+app.use(express.json())
 
 app.get("/", function(req, res) {
     res.send("Minha primeira requisição")
@@ -14,9 +17,24 @@ app.get("/com-parametros", function(req, res) {
     if (req.query.nome === "Jennifer") {
         res.send("Jennifer chamou requisição")
     }
-    res.send("Com parâmetros funciona! Sabadou, " + req.query.nome + req.query.sobrenome)
+    res.send("Com parâmetros funciona! Sabadou, " + req.query.nome + " " + req.query.sobrenome)
+})
+
+app.post("/meu-primeiro-post", function(req, res) {
+    console.log(req.body);
+    res.send("Meu post funciona")
+})
+
+app.put("/meu-primeiro-put/:id", function(req, res) {
+    console.log(req.body, req.params.id)
+    res.send("Meu put funciona")
+})
+
+app.delete("/meu-primeiro-delete/:id", function(req, res) {
+    console.log(req.params.id)
+    res.send("Meu delete funciona " + req.params.id)
 })
 
 app.listen(port, function() {
     console.log("Ouvindo a porta: ", port);
-})
+}) 
